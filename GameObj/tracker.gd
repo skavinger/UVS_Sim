80f5613bde297520f96bcc0128c04f0eda353369
skavinger <skavinger@gmail.com>
2025-10-08 14:10:57 -0700

    Add code for block status

--- GameObj/tracker.gd
+++ GameObj/tracker.gd
@@ -8,3 +8,22 @@ var damage
 func _ready() -> void:
 	speed = 4
 	damage = 4
+
+
+func _on_full_block_pressed() -> void:
+	resetTracker()
+
+func _on_partial_block_pressed() -> void:
+	$"../../../SyncFunctions".loseHealth(int(ceil(float(damage) / 2)))
+	resetTracker()
+
+
+func _on_unblocked_pressed() -> void:
+	$"../../../SyncFunctions".loseHealth(damage)
+	resetTracker()
+
+func resetTracker():
+	speed = 4
+	damage = 4
+	$SpeedVal/SpeedLabel.text = str(4)
+	$DamageVal/DamageLabel.text = str(4)
